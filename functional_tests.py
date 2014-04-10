09003d2f5ecacb16ee5dd4640c454b1693656018
Peter Kim <peter@peter-VirtualBox.(none)>
2014-04-10 16:11:13 +0900

    Adding helper function for the table

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 	
+	# helper method
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows] )
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('To-Do', self.browser.title)
@@ -26,18 +32,15 @@ class NewVisitorTest(unittest.TestCase):
 				'Enter a to-do item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER) # import Keys
-		
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		#self.assertTrue(
-		#		any(row.text == '1: Buy peacock feathers' for row in rows),
-		#		"New to-do item did not appear in table -- its text was:\n{}".format(table.text) )
-		# revised and modified
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feather to make a fly', 
-				[row.text for row in rows] )
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		# as a reminder to finish the test
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make fly')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make fly') 
+		
+		#as a reminder to finish the test
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
