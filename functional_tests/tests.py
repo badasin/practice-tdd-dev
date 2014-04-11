e30625a6388e3d3c5fa664e9f0ba8fcfab4a9b91
Peter Kim <peter@peter-VirtualBox.(none)>
2014-04-11 15:26:18 +0900

    Adding a test case for a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,6 +34,8 @@ class NewVisitorTest(LiveServerTestCase): # instead of <unittest.TestCase>
 				'Enter a to-do item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER) # import Keys
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -41,6 +43,26 @@ class NewVisitorTest(LiveServerTestCase): # instead of <unittest.TestCase>
 		inputbox.send_keys(Keys.ENTER)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make fly') 
+		
+		# new user
+		self.browser.quit()
+		self.browser = webdriver.FireFox()
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 		
 		#as a reminder to finish the test
 		self.fail('Finish the test!')
