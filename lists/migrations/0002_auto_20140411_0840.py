cf1f7c997188c22cd323ee80d507c65c439c843a
Peter Kim <peter@peter-VirtualBox.(none)>
2014-04-11 17:48:10 +0900

    3 changed files, plus 2 migrations, List model

---
+++ lists/migrations/0002_auto_20140411_0840.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', default=1, to_field='id'),
+            preserve_default=False,
+        ),
+    ]
