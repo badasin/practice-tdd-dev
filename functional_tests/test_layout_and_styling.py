2df8f97755cc0c044c673bad1b69e5f519c70050
Peter Kim <gobbyjwk@hotmail.com>
2014-04-15 16:14:04 +0900

    Adjust new list view to do model validation

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -2,6 +2,7 @@
 from .base import FunctionalTest
 
 class LayoutAndStylingTest(FunctionalTest):
+	
 	def test_layout_and_styling(self):
 		#self.browser.get(self.live_server_url)
 		self.browser.get(self.server_url)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.utils.html import escape
 from lists.views import home_page
 from lists.models import Item, List
 
@@ -65,6 +66,20 @@ class NewListTest(TestCase):
 				'/lists/new', data={'item_text': 'A new list item'})
 		new_list = List.objects.first()
 		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+	def test_validation_errors_are_sent_back_to_home_page_template(self):
+		response = self.client.post('/lists/new', data={'item_text': ''})
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'home.html')
+		# Django helper escape() is used because of "'" as "&#39;" is printed
+		expected_error = escape("You can't have an empty list item.")
+		self.assertContains(response, expected_error)
+
+	def test_invalid_list_items_arnt_saved(self):
+		# make sure objects with invalid inputs are not created
+		self.client.post('/lists/new', data={'item_text': ''})
+		self.assertEqual(List.objects.count(), 0)
+		self.assertEqual(Item.objects.count(), 0)
 
 
 
