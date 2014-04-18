6fb04cdace1a42edee1f81ca40e36016199a4508
Peter Kim <gobbyjwk@hotmail.com>
2014-04-18 17:35:15 +0900

    Adding Accounts and mock testing with sinon

--- superlists/settings.py
+++ superlists/settings.py
@@ -44,20 +44,24 @@ INSTALLED_APPS = (
 )
 
 # new for authentication
-AUTH_USER_MODEL = 'accounts.ListUser'
-AUTHENTICATION_BACKENDS = (
-		'accounts.authentication.PersonaAuthenticationBackend',
-)
+#AUTH_USER_MODEL = 'accounts.ListUser'
+#AUTHENTICATION_BACKENDS = (
+#		'accounts.authentication.PersonaAuthenticationBackend',
+#)
 
 LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
 	'handlers': {
-			'console': 'DEBUG',
-			'class': 'logging.StreamHandler',
+			'console': {
+				'level': 'DEBUG',
+				'class': 'logging.StreamHandler',
+			},
 	},
 	'loggers': {
-			'django': { 'handlers': ['console'], },
+			'django': { 
+					'handlers': ['console'], 
+			},
 	},
 	'root': { 'level': 'INFO' },
 }
@@ -107,4 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'superlists', 'static'),
+)
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'superlists.views.home', name='home'),
 		url(r'^$', 'lists.views.home_page', name='home'),
 		url(r'^lists/', include('lists.urls')),
-		url(r'^accounts/', include('accounts.urls')),
+		#url(r'^accounts/', include('accounts.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
