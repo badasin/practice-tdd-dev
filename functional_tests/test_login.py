8b55b68d7a439ac8db02668490eaa7361b5709f4
Peter Kim <gobbyjwk@hotmail.com>
2014-04-19 16:23:49 +0900

    FT for login and logout

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -19,11 +19,26 @@ class LoginTest(FunctionalTest):
 		self.wait_for_element_with_id('id_logout')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertIn('edith@mockmyid.com', navbar.text)
-	
+		
+		# login and logout
+		self.switch_to_new_window('To-Do')
+		self.wait_to_be_logged_in()
+		self.browser.refresh()
+		self.wait_to_be_logged_in()
+		self.browser.find_element_by_id('id_logout').click()
+		self.wait_to_be_logged_out()
+		self.browser.refresh()
+		self.wait_to_be_logged_out()
+
+
+
 	# using selenuim WebDriverWait for 'explicit wait'
 	def wait_for_element_with_id(self, element_id):
 		WebDriverWait(self.browser, timeout=30).until(
-				lambda b: b.find_element_by_id(element_id)
+				lambda b: b.find_element_by_id(element_id),
+				'Could not find element with if {}. Page text was was {}'.format(
+					element_id, self.browser.find_element_by_tag_name('body').text
+				)
 		)
 
 	# helper
@@ -37,3 +52,16 @@ class LoginTest(FunctionalTest):
 			retries -= 1
 			time.sleep(0.5)
 		self.fail('Could not find window')
+
+	def wait_to_be_logged_in(self):
+		self.wait_for_element_with_id('id_logout')
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertIn('edith@mockmyid.com', navbar.text)
+	
+	def wait_to_be_logged_out(self):
+		self.wait_for_element_with_id('id_login')
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertNotIn('edith@mockmyid.com', navbar.text)
+			
+
+
