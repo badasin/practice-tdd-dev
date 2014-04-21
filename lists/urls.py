7328a4b249a96ea21a7d55ab4ba81f350553ebf2
Peter Kim <gobbyjwk@hotmail.com>
2014-04-21 14:56:03 +0900

    URL, placeholder view, and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -6,5 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
 		url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
 		url(r'^new$', 'lists.views.new_list', name='new_list'),
-		#url(r'^(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
+		url(r'^users/(.+)/$', 'lists.views.my_lists', name='my_lists'),
 )
